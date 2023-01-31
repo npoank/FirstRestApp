bd73d3e3826dbacd604e6433d9e176d02882e1a6
Kulbicki Andrey <kubl-ml@mail.ru>
2023-01-31 10:42:01 +0300

    REST app

---
+++ src/main/java/com/andrew/FirstRestApp/controllers/PeopleController.java
@@ -0,0 +1,33 @@
+package com.andrew.FirstRestApp.controllers;
+
+import com.andrew.FirstRestApp.models.Person;
+import com.andrew.FirstRestApp.services.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping
+    public List<Person> getPeople(){
+        return peopleService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") int id){
+        return peopleService.findPerson(id);
+    }
+}
